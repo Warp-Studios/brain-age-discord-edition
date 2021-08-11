8446da5b15e2050bffae1ae0ae6a37bc909eccfc
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-11 18:39:55 -0400

    Move to the games folder

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ load_dotenv()
 bot = Bot(command_prefix="b!")
 slash = SlashCommand(bot)
 
-bot.load_extension("3ds.concentration_training")
+bot.load_extension("games.3ds")
 bot.load_extension("other.owner")
 
 bot.run(environ.get("TOKEN"))
