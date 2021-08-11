a114e8c1c50a5bdb3200a50aec17c8f6c319d6e2
b1ue64 <77976308+b1ue64@users.noreply.github.com>
2021-08-11 15:21:38 -0400

    remove slash commands and change prefix

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ from discord_slash.client import SlashCommand
 
 load_dotenv()
 
-bot = Bot(command_prefix="Hey Dr. Ryuta ")
-slash = SlashCommand(bot)
+bot = Bot(command_prefix="b!")
 
 bot.load_extension("3ds.concentration_training")
 
