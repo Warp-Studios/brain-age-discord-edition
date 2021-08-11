9ac42bc43d00325cf592300b213b33d1fef62241
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-11 18:42:49 -0400

    Tidy up imports

--- games/3ds.py
+++ games/3ds.py
@@ -3,31 +3,17 @@ Platform: 3DS
 Title: Brain Age: Concentration Training
 '''
 
-from discord.ext import commands
-from discord import Embed
-from discord_slash.utils.manage_components import create_button, create_actionrow
-from discord_slash.model import ButtonStyle
-from random import randint
 from asyncio import sleep
 from copy import deepcopy
+from random import randint
+
+from discord.ext import commands
+
 
 class ConcentrationTraining(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.games = {}
-
-    @commands.command()
-    async def play(self, ctx):
-        """
-        Opens the menu
-        """
-        embed=Embed(title="Welcome!", description="Hi, I'm Dr. Ryuta Kawashima, and I study the brain at a top university in Japan. Select an option below to get training!", color=0x80ff00)
-        embed.set_author(name="Dr. Ryuta Kawashima", url="https://en.wikipedia.org/wiki/Ryuta_Kawashima", icon_url="https://www.giantbomb.com/a/uploads/scale_medium/0/4362/190766-dr_kawashima.jpg")
-        embed.set_footer(text="Brain Age Discord Edition designed by JG#3205")
-        buttons = [create_actionrow(*[
-            create_button(style=ButtonStyle.blurple, label="Devilish Calculations", custom_id="dc"),
-        ])]
-        await ctx.send(embed=embed, components=buttons)
     @commands.Cog.listener()
     async def on_component(self, ctx):
         await ctx.defer()
