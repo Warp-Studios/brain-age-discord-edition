fb5d2c0ed079d957f974491f683a92e177d59b24
zurgeg <46549042+zurgeg@users.noreply.github.com>
2021-08-11 18:44:32 -0400

    Add load and unload

--- other/owner.py
+++ other/owner.py
@@ -16,6 +16,24 @@ class Owner(commands.Cog):
             await ctx.send(f"Successfully reloaded {cog}")
         except Exception as e:
             await ctx.send(f"Error reloading {cog}: ```py\n{format_exc()}```")
+    @commands.command()
+    @is_owner()
+    async def load(self, ctx, *, cog: str):
+        """Loads a cog"""
+        try:
+            self.bot.load_extension(f"{cog}")
+            await ctx.send(f"Successfully reloaded {cog}")
+        except Exception as e:
+            await ctx.send(f"Error loading {cog}: ```py\n{format_exc()}```")
+    @commands.command()
+    @is_owner()
+    async def unload(self, ctx, *, cog: str):
+        """Unloads a cog"""
+        try:
+            self.bot.unload_extension(f"{cog}")
+            await ctx.send(f"Successfully unloaded {cog}")
+        except Exception as e:
+            await ctx.send(f"Error unloading {cog}: ```py\n{format_exc()}```")
 
 def setup(bot):
     bot.add_cog(Owner(bot))
